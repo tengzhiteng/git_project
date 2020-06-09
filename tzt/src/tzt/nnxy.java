2dc3ff95b212b8c97031b659f4a77cdb489db55e
tengzhiteng <136959245@qq.com>
2020-06-09 15:50:57 +0800

    提交到仓库

---
+++ tzt/src/tzt/nnxy.java
@@ -0,0 +1,21 @@
+package tzt;
+
+public class nnxy {
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public nnxy(int id, String username, String password, int sex, String love) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.sex = sex;
+		this.love = love;
+	}
+	
+
+	
+}
